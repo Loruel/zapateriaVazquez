29709a101abef28da07e5d9dbecbe8786226731c
Loruel <paco.jeansl@gmail.com>
2024-09-04 08:54:19 -0600

    correccion promocion2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Filter from '../components/filter/Filter'
 import MasVendidos from '../components/body/MasVendidos'
 import FooterMarcas from '../components/footer/FooterMarcas'
 import Redes from '../components/footer/Redes'
-import Promocion from '../components/body/promocion'
+import Promocion from '../components/body/Promocion'
 import Footer from '../components/footer/Footer'
 
 export default function HomePage() {
